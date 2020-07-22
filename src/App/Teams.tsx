34b5dce48f94a57e7b53ecba8beaf891355233c5
Jae Bradley <jae.b.bradley@gmail.com>
2020-07-22 17:31:08 -0400

    Initial team searching

--- src/App/Teams.tsx
+++ src/App/Teams.tsx
@@ -1,16 +1,37 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import Fuse from 'fuse.js';
 
 import TEAMS from '@Src/teams';
 import TeamLogo from '@App/TeamLogo';
 
+const TEAM_VALUES = Array.from(TEAMS.entries()).map(([, team]) => team);
+const fuse = new Fuse(TEAM_VALUES, { keys: ['name', 'abbreviation'] });
+
 function Teams(): React.ReactElement {
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const filteredTeams = React.useMemo(() => {
+    if (searchTerm.trim()) {
+      return fuse.search(searchTerm).map((match) => ({
+        ...match.item,
+      }));
+    }
+
+    return TEAM_VALUES;
+  }, [searchTerm]);
+
+  const handleSearchTermChange = React.useCallback((event) => setSearchTerm(event.target.value), [setSearchTerm]);
+
   return (
-    <StyledTeams>
-      {Array.from(TEAMS.entries()).map(([, team]) => (
-        <TeamLogo key={team.id} team={team} size="5rem" />
-      ))}
-    </StyledTeams>
+    <>
+      <input type="text" onChange={handleSearchTermChange} />
+      <StyledTeams>
+        {filteredTeams.map((team) => (
+          <TeamLogo key={team.id} team={team} size="5rem" />
+        ))}
+      </StyledTeams>
+    </>
   );
 }
 
