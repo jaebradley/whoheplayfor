5895848bc500ffb2cf28cc86f28fd77c7cfb63b0
Jae Bradley <jae.b.bradley@gmail.com>
2020-07-23 17:47:26 -0400

    Add player image fallback (#9)

--- src/App/PlayerImage.tsx
+++ src/App/PlayerImage.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import { Icon } from 'react-icons-kit';
+import { ic_person } from 'react-icons-kit/md/ic_person';
 
 function PlayerImage({ playerId }: { playerId: number }): React.ReactElement {
   const url = `https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/${playerId}.png`;
   return (
     <StyledLink>
-      <StyledImage src={url} />
+      <StyledObject data={url} type="image/png">
+        <Icon size="5rem" icon={ic_person} />
+      </StyledObject>
     </StyledLink>
   );
 }
@@ -17,7 +21,7 @@ const StyledLink = styled.a`
   display: block;
 `;
 
-const StyledImage = styled.img`
+const StyledObject = styled.object`
   max-width: 10rem;
 `;
 
