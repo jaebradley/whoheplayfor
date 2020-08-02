57370454221e83daf91abb06698658c2365c5386
Jae Bradley <jae.b.bradley@gmail.com>
2020-08-02 19:09:36 -0400

    Remove header icons (#43)

--- src/App/Header.tsx
+++ src/App/Header.tsx
@@ -2,10 +2,6 @@ import * as React from 'react';
 import styled from 'styled-components';
 import * as PropTypes from 'prop-types';
 import { InferProps } from 'prop-types';
-import { Icon } from 'react-icons-kit';
-import { ic_home } from 'react-icons-kit/md/ic_home';
-import { ic_info } from 'react-icons-kit/md/ic_info';
-import { ic_replay } from 'react-icons-kit/md/ic_replay';
 
 import theme, { ThemeInterface } from '@App/styles/theme';
 import GitHubLogo from '@Images/github.svg';
@@ -13,11 +9,6 @@ import GitHubLogo from '@Images/github.svg';
 function Header({ className }: InferProps<typeof Header.propTypes>): React.ReactElement {
   return (
     <StyledHeader className={className || ''} theme={theme}>
-      <StyledIcons>
-        <StyledIcon size="1.75rem" icon={ic_home} />
-        <StyledIcon size="1.75rem" icon={ic_info} />
-        <StyledIcon size="1.75rem" icon={ic_replay} />
-      </StyledIcons>
       <a href="https://github.com/jaebradley/whoheplayfor" target="_blank" rel="noreferrer">
         <StyledGithubLogo />
       </a>
@@ -29,19 +20,8 @@ const StyledHeader = styled.header<{ theme: ThemeInterface }>`
   background-color: ${({ theme }) => theme.primary};
   display: flex;
   height: 4rem;
-  justify-content: space-between;
+  justify-content: flex-end;
   padding: 0 1rem 0;
-`;
-
-const StyledIcons = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 7rem;
-`;
-
-const StyledIcon = styled(Icon)`
-  color: ${({ theme }) => theme.secondary};
-  cursor: pointer;
 `;
 
 const StyledGithubLogo = styled(GitHubLogo)`
